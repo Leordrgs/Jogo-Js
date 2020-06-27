2eb876b13a3510921994aa10c4557b71cfde02b9
Leonardo <leonardo.rodrigues@catolicasc.org.br>
2020-06-26 21:55:32 -0300

    Bola batendo e voltando na barra

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext("2d");
 
 //Bola
 let x = canvas.width / 2; // inicial horizontal
-let y = canvas.height - 30; // inicial vertical
+let y = canvas.height - 35; // inicial vertical
 let dx = 2; // variação horizontal
 let dy = -2; // variação vertical
 let ballRadius = 10; // raio da bola
@@ -12,7 +12,7 @@ let ballRadius = 10; // raio da bola
 let paddleHeight = 10;
 let paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
-let paddleY = (canvas.height - paddleHeight);
+let paddleY = (canvas.height - paddleHeight) - 10;
 let rightPressed =  false;
 let leftPressed = false;
 
@@ -32,6 +32,7 @@ function drawBall() {
     ctx.closePath();
 }
 
+let interval = setInterval(draw, 10);
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
@@ -42,8 +43,14 @@ function draw(){
         dx = -dx; // inverte o sinal de dx
     }
     // verifica se a bola sai na vertical
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+    if(y + dy < ballRadius){
         dy = -dy; // inverte o sinal de dy
+    }else if(x > paddleX && x < paddleX + paddleY && y + ballRadius >= paddleY){
+        dy = -dy;
+    }else if(y + dy > canvas.height-ballRadius){
+        alert ("Game Over!");
+        document.location.reload();
+        clearInterview(interval);
     }
 
     if(rightPressed){
@@ -61,7 +68,7 @@ function draw(){
     x += dx;
     y += dy;
 }
-setInterval(draw, 10);
+
 
 function keyDownHandler(e){
     if(e.key == "Right" || e.key == "ArrowRight"){
